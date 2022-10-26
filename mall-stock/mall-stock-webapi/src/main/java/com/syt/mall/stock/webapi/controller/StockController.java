5ab4bc59410207aa35ca503d13306b11afbaf355
lingnian <sytsnb@gmail.com>
2022-10-26 17:38:06 +0800

    sentinel-哨兵

--- mall-stock/mall-stock-webapi/src/main/java/com/syt/mall/stock/webapi/controller/StockController.java
+++ mall-stock/mall-stock-webapi/src/main/java/com/syt/mall/stock/webapi/controller/StockController.java
@@ -1,5 +1,6 @@
 package com.syt.mall.stock.webapi.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.syt.mall.commons.pojo.stock.dto.StockReduceCountDTO;
 import com.syt.mall.commons.restful.JsonResult;
 import com.syt.mall.stock.service.IStockService;
@@ -25,8 +26,18 @@ public class StockController {
     @Autowired
     private IStockService stockService;
 
+    /**
+     * -@SentinelResource注解需要写到控制层方法上
+     * 在该方法运行后,会被Sentinel监测
+     * 该方法运行前,Sentinel检测不到,至少必须运行一次后才可以
+     * "减少商品库存数"-就是显示在Sentinel上的名称
+     *
+     * @param stockReduceCountDTO
+     * @return
+     */
     @ApiOperation("减少库存")
     @PostMapping("/reduce/count")
+    @SentinelResource("减少商品库存数")
     public JsonResult stockReduce(StockReduceCountDTO stockReduceCountDTO) {
         stockService.reduceCommodityCount(stockReduceCountDTO);
         return JsonResult.ok("库存减少成功");
