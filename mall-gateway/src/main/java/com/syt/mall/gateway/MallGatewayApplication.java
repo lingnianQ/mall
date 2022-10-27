f3fa918a3d554bc5d0e7c4f7a51407949b593d93
lingnian <sytsnb@gmail.com>
2022-10-27 15:32:53 +0800

    gateway

---
+++ mall-gateway/src/main/java/com/syt/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.syt.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sytsn
+ */
+@SpringBootApplication
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
